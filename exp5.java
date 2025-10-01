265d85c081f0f2dcd2eb0ce3857785c753e52bab
devilevil23 <dhruvsinghvns5@gmail.com>
2025-10-01 17:16:09 +0530

    Update exp5.java

--- exp5.java
+++ exp5.java
@@ -1,3 +1,20 @@
-public class exp5 {
-    
+import java.util.Scanner;
+import java.util.ArrayList;
+public class exp5{
+    public static void main(String[] args){
+
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Integer> list =new ArrayList<>();
+
+        for(int i=0;i<3;i++){
+            System.out.println("Enter value of index:"+i);
+            int num=sc.nextInt();
+            list.add(num);
+        }
+        int sum=0;
+        for(Integer ele:list){
+            sum+=ele;
+        }
+        System.out.println("Sum:"+sum);
+    }
 }
